ea103f0182cf9eef302d5ea36ea8c989d33f0f38
vinitathopte2603 <vinitathopte1@outlook.com>
2020-05-04 15:10:34 +0530

    [Vinita]ADDED:PlayerChecksForOptions

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -6,4 +6,18 @@ echo "$var"
 INITIAL_POSITION=0;
 NUMBEROFPLAYER=1;
 
+firstPlayerPosition=$INITIAL_POSITION;
+NOPLAY=0;
+LADDER=1;
+SNAKE=2; 
 randomDieMove=$((RANDOM%6+1))
+Option=$((RANDOM%3))
+
+	case $Option in
+	$NOPLAY)
+		firstPlayerPosition=$firstPlayerPosition;;
+	$LADDER)
+		firstPlayerPosition=$(( $firstPlayerPosition + $randomDieMove ));;
+	$SNAKE)
+		firstPlayerPosition=$(( $firstPlayerPosition - $randomDieMove ));;
+	esac
