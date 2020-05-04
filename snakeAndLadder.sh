1373ede11a75b9ba82e53eb4118edaa44b975950
vinitathopte2603 <vinitathopte1@outlook.com>
2020-05-04 15:22:55 +0530

    [Vinita]ADDED:RepeatPlayingTillPlayerWins

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -4,20 +4,42 @@ var="Welcome to snake and ladder game"
 echo "$var"
 
 INITIAL_POSITION=0;
+WINNING_POSITION=100;
 NUMBEROFPLAYER=1;
-
 firstPlayerPosition=$INITIAL_POSITION;
 NOPLAY=0;
 LADDER=1;
 SNAKE=2; 
-randomDieMove=$((RANDOM%6+1))
-Option=$((RANDOM%3))
 
+
+function moveCondition(){
+
+	if [ $1 -le 0 ]
+	then
+		echo $1=$INITIAL_POSITION;
+	elif [ $1 -ge $WINNING_POSITION ]
+	then
+		echo win;
+	fi 
+}
+
+while [ $firstPlayerPosition -le $WINNING_POSITION ]
+do
+	randomDieMove=$((RANDOM%6+1))
+	Option=$((RANDOM%3))
 	case $Option in
 	$NOPLAY)
-		firstPlayerPosition=$firstPlayerPosition;;
+		firstPlayerPosition=$firstPlayerPosition
+		;;
+
 	$LADDER)
-		firstPlayerPosition=$(( $firstPlayerPosition + $randomDieMove ));;
+		firstPlayerPosition=$(( $firstPlayerPosition + $randomDieMove ))
+		;;
 	$SNAKE)
-		firstPlayerPosition=$(( $firstPlayerPosition - $randomDieMove ));;
+		firstPlayerPosition=$(( $firstPlayerPosition - $randomDieMove ))
+		;;
+
 	esac
+	 moveCondition $firstPlayerPosition
+
+done
